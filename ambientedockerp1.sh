0bdb970a7ec25ad1d56c06194deed32d7f1efea2
Ubuntu <ubuntu@ip-172-31-83-190.ec2.internal>
2022-11-01 18:04:25 +0000

    creacion de volumen y try:Levantar contenedor postgress

--- ambientedockerp1.sh
+++ ambientedockerp1.sh
@@ -1,1 +1,4 @@
 docker network create network0
+docker volume create volumen_pdz1
+
+
---
+++ dbcontainer_create.sh
@@ -0,0 +1,2 @@
+docker run --rm --name postgres_pdz --network network0 --volume volumen_pdz1:/var/lib/postgresql/data -p 5432:5432 --env
+POSTGRES_DB=db_pdz --env POSTGRES_USER=all --env POSTGRES_PASSWORD=none
