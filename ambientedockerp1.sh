dcd659e58c7a1cfbe4acb2fc1cd91c9a33bef282
Ubuntu <ubuntu@ip-172-31-83-190.ec2.internal>
2022-11-02 17:07:28 +0000

    Network,Volumen and DB container ready

--- ambientedockerp1.sh
+++ ambientedockerp1.sh
@@ -1,4 +1,4 @@
 docker network create network0
 docker volume create volumen_pdz1
-
+docker run --rm --name postgres_pdz --network network0 --volume volumen_pdz1:/var/lib/postgresql/data -p 5432:5432 --env POSTGRES_DB=db_pdz --env POSTGRES_USER=all --env POSTGRES_PASSWORD=none postgres
 
--- dbcontainer_create.sh
+++ dbcontainer_create.sh
@@ -1,2 +1,1 @@
-docker run --rm --name postgres_pdz --network network0 --volume volumen_pdz1:/var/lib/postgresql/data -p 5432:5432 --env
-POSTGRES_DB=db_pdz --env POSTGRES_USER=all --env POSTGRES_PASSWORD=none
+docker run --rm --name postgres_pdz --network network0 --volume volumen_pdz1:/var/lib/postgresql/data -p 5432:5432 --env POSTGRES_DB=db_pdz --env POSTGRES_USER=all --env POSTGRES_PASSWORD=none postgres
